c2feb5a5392c84d63a4bb845cec28ee86bb6a091
NandanaMV <85772239+NandanaMV@users.noreply.github.com>
2025-05-12 04:08:10 +0800

    modifed test data to reflect friendships correclty (bidirectional data)

--- add_test_data.py
+++ add_test_data.py
@@ -26,8 +26,9 @@ def add_test_data():
         db.session.commit()
         
         # Create friendship between users
-        friendship = Friendship(user_id=test_user1.username, friend_id=test_user2.username)
-        db.session.add(friendship)
+        friendship1 = Friendship(user_id=test_user1.username, friend_id=test_user2.username)
+        friendship2 = Friendship(user_id=test_user2.username, friend_id=test_user1.username)
+        db.session.add_all([friendship1, friendship2])
 
         # Create friend request from test_user3 to test_user1
         friend_request1 = FriendRequest(sender_id=test_user3.username, receiver_id=test_user1.username, status='pending')
