48cce4a053c24bd06fdc2271d8b34fb63b63a938
kambytes <itskamleshkumar.s@gmail.com>
2025-05-15 11:53:52 +0800

    A new ChangePasswordForm class has been added into forms.py. This will be necessary for CSRF protection.

--- app/forms.py
+++ app/forms.py
@@ -16,3 +16,8 @@ class SignupForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     remember = BooleanField('Remember Me')
+
+class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField('Current Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[DataRequired(), Length(min=6)])
+    confirm_new_password = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('new_password')])
